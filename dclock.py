eb2c081219e1ac37fd591728a82a4f035b65b8d4
DENCHU <spam@denchu.jp>
2020-09-07 18:01:15 +0900

    画面中央に出るように変更。ターミナルサイズが小さいときは拡大しないようにした

--- dclock.py
+++ dclock.py
@@ -71,18 +71,25 @@ def P(stdscr, msg):
     stdscr.refresh()
     stdscr.getkey()
 
-def view_dot(stdscr, xs, dot, scale):
+def view_dot(stdscr, xs, max_height, dot, scale):
     width = 5 * scale
     height = 5 * scale
+    ys = floor((max_height - height) / 2 + 0.5)
     for y in range(0, int(height)):
         for x in range(0, int(width)):
             xn = floor((x + 0.5) / scale)
             yn = floor((y + 0.5) / scale)
             ch = dot[yn][xn]
-            stdscr.addch(y, xs + x, ch)
+            stdscr.addch(ys + y, xs + x, ch)
 
-def view_num(stdscr, xs, num, scale):
-    view_dot(stdscr, xs, NUM_DOT[num], scale)
+def view_num(stdscr, xs, max_height, num, scale):
+    view_dot(stdscr, xs, max_height, NUM_DOT[num], scale)
+
+def view_normal(stdscr, max_width, max_height, now):
+    xs = floor((max_width - 8) / 2 + 0.5)
+    ys = floor((max_height - 5) / 2 + 0.5)
+    msg = now.strftime('%H:%M:%S')
+    stdscr.addstr(ys, xs, msg)
 
 def main(stdscr):
     stdscr.nodelay(True)
@@ -96,14 +103,17 @@ def main(stdscr):
             hour = now.hour
             minutes = now.minute
             second = now.second
-            view_num(stdscr, 0, hour // 10, scale)
-            view_num(stdscr, num_width, hour % 10, scale)
-            view_dot(stdscr, num_width * 2, COLON_DOT, scale)
-            view_num(stdscr, num_width * 3, minutes // 10, scale)
-            view_num(stdscr, num_width * 4, minutes % 10, scale)
-            view_dot(stdscr, num_width * 5, COLON_DOT, scale)
-            view_num(stdscr, num_width * 6, second // 10, scale)
-            view_num(stdscr, num_width * 7, second % 10, scale)
+            if scale < 1.0:
+                view_normal(stdscr, width, height, now)
+            else:
+                view_num(stdscr, 0, height, hour // 10, scale)
+                view_num(stdscr, num_width, height, hour % 10, scale)
+                view_dot(stdscr, num_width * 2, height, COLON_DOT, scale)
+                view_num(stdscr, num_width * 3, height, minutes // 10, scale)
+                view_num(stdscr, num_width * 4, height, minutes % 10, scale)
+                view_dot(stdscr, num_width * 5, height, COLON_DOT, scale)
+                view_num(stdscr, num_width * 6, height, second // 10, scale)
+                view_num(stdscr, num_width * 7, height, second % 10, scale)
             stdscr.refresh()
             ch = stdscr.getch()
             if ch == ord('q'):
